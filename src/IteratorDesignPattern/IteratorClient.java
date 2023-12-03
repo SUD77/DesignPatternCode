464d56381854bf826e6f9588e862820f50f81e3b
sudhanshujaiswal <jaiswalsudhanshu20@gmail.com>
2023-12-03 17:50:03 +0530

    Added iterator design pattern

---
+++ src/IteratorDesignPattern/IteratorClient.java
@@ -0,0 +1,42 @@
+package IteratorDesignPattern;
+
+import java.util.Iterator;
+
+public class IteratorClient {
+
+    public static void main(String[] args) {
+
+        /*
+        * So, Client defines both the collection and its Iteration method and then
+        * uses the both.
+        * For example : We as cleint define the collection we want to use i.e arraylist, hashmap
+        * and then we store the data and also, for iteration, we define the iterator too.
+        * */
+
+
+        SongCollection songCollection = new SongCollection();
+        songCollection.addSong("Song 1");
+        songCollection.addSong("Song 2");
+        songCollection.addSong("Song 3");
+
+        PodcastCollection podcastCollection = new PodcastCollection();
+        podcastCollection.addPodcast("Podcast 1");
+        podcastCollection.addPodcast("Podcast 2");
+        podcastCollection.addPodcast("Podcast 3");
+
+        // Using SongIterator
+        Iterator<String> songIterator = songCollection.createIterator();
+        while (songIterator.hasNext()) {
+            String song = songIterator.next();
+            System.out.println("Playing song: " + song);
+        }
+
+        // Using PodcastIterator
+        Iterator<String> podcastIterator = podcastCollection.createIterator();
+        while (podcastIterator.hasNext()) {
+            String podcast = podcastIterator.next();
+            System.out.println("Playing podcast: " + podcast);
+        }
+    }
+
+}
---
+++ src/IteratorDesignPattern/SongCollection.java
@@ -0,0 +1,25 @@
+package IteratorDesignPattern;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class SongCollection implements MediaCollection{
+
+    /*
+    * Here, data type can be on anytype, we dont have to use List. It can be array, hashmap, etc
+    * And then out iterator will behave accordingly and we will have to impelement the iteration
+    * logic accordingly.
+    * */
+    private List<String> songs=new ArrayList<>();
+
+    public void addSong(String song){
+        songs.add(song);
+    }
+
+    // This method will return our Custom Iterator for SongCollection.
+    @Override
+    public Iterator<String> createIterator() {
+        return new SongIterator(songs);
+    }
+}
